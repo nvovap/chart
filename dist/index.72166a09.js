b230c2d24cc4dd0130435381ce47b3e71f827f18
nvovap <nvovap@gmail.com>
2021-04-15 18:20:53 +0300

    6 block

--- dist/index.72166a09.js
+++ dist/index.72166a09.js
@@ -789,6 +789,7 @@ function sliderChart(root, data, DPI_WIDTH) {
   const WIDTH = DPI_WIDTH / 2;
   const VIEW_WIDTH = DPI_WIDTH;
   const VIEW_HEIGHT = DPI_HEIGHT;
+  const MIN_WIDTH = WIDTH * 0.05;
   const canvas = root.querySelector('canvas');
   const ctx = canvas.getContext("2d");
   canvas.style.width = WIDTH + 'px';
@@ -797,6 +798,40 @@ function sliderChart(root, data, DPI_WIDTH) {
     width: WIDTH + 'px',
     height: HEIGHT + 'px'
   });
+  const $left = root.querySelector('[data-type="left"]');
+  const $zoom_window = root.querySelector('[data-type="window"]');
+  const $right = root.querySelector('[data-type="right"]');
+  root.addEventListener("mousedown", mousedown);
+  function mousedown(event) {}
+  const defaultWidth = WIDTH * 0.3;
+  setPosition(0, defaultWidth);
+  function setPosition(left, right) {
+    const w = WIDTH - right - left;
+    if (w < MIN_WIDTH) {
+      _utils.css($zoom_window, {
+        width: MIN_WIDTH + 'px'
+      });
+      return;
+    }
+    if (left < 0) {
+      _utils.css($zoom_window, {
+        left: '0px'
+      });
+      _utils.css($left, {
+        width: '0px'
+      });
+      return;
+    }
+    if (right < 0) {
+      _utils.css($zoom_window, {
+        right: '0px'
+      });
+      _utils.css($right, {
+        width: '0px'
+      });
+      return;
+    }
+  }
   canvas.width = DPI_WIDTH;
   canvas.height = DPI_HEIGHT;
   const [yMin, yMax] = _utils.computeBounderies(data);
--- src/slider.js
+++ src/slider.js
@@ -10,6 +10,8 @@ export function sliderChart(root, data, DPI_WIDTH) {
     const VIEW_WIDTH = DPI_WIDTH
     const VIEW_HEIGHT = DPI_HEIGHT
 
+    const MIN_WIDTH = WIDTH * 0.05
+
     const canvas = root.querySelector('canvas')
     const ctx = canvas.getContext("2d")
 
@@ -20,6 +22,45 @@ export function sliderChart(root, data, DPI_WIDTH) {
         width: WIDTH + 'px',
         height: HEIGHT + 'px',
     })
+
+    const $left = root.querySelector('[data-type="left"]')
+    const $zoom_window = root.querySelector('[data-type="window"]')
+    const $right = root.querySelector('[data-type="right"]')
+
+    root.addEventListener("mousedown", mousedown)
+
+    function mousedown(event) {
+        
+    }
+
+
+    const defaultWidth = WIDTH * 0.3
+
+    setPosition(0, defaultWidth)
+
+
+    function setPosition(left, right) {
+        const w = WIDTH - right - left
+
+        if (w < MIN_WIDTH ) {
+            css($zoom_window, {width: MIN_WIDTH + 'px'})
+            return
+        }
+
+        if (left < 0) {
+            css($zoom_window, { left:'0px' })
+            css($left, { width:'0px' })
+            return
+        }
+
+        if (right < 0) {
+            css($zoom_window, { right:'0px' })
+            css($right, { width:'0px' })
+            return
+        }
+
+        
+    }
 
     canvas.width = DPI_WIDTH
     canvas.height = DPI_HEIGHT
